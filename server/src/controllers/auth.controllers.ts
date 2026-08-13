1f2ead2b033144ab0dbac54a38f83070c407e47a
Adan Adeel <adanadeel903@gmail.com>
2026-08-13 13:22:01 +0500

    Working on Auth Controller (Register Handler )

---
+++ server/src/controllers/auth.controllers.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import { User } from "../models/user.models.js";
+import { Request, Response } from "express";
+
+
+function registerHandler(req :Request, res:Response) { 
+    try { 
+
+    } catch(err) { 
+
+    }
+}
